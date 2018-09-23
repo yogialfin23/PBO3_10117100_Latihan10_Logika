33d3b5182782fcbf35c3d8b53f26d34965e3178b
yogialfin23 <43507570+yogialfin23@users.noreply.github.com>
2018-09-23 18:45:28 +0700

    Add files via upload

---
+++ PBO3_10117100_Latihan10_Logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117100_latihan10_logika;
+
+/**
+ * Yogie Alfin Salim
+ * 10117100
+ * IF-3
+ */
+public class PBO3_10117100_Latihan10_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
